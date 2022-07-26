ccca8b92a1b33462490012f8dbeb8582547fc0f4
Ankit Ostwal <ostwalankit@gmail.com>
2022-07-26 13:34:18 +0530

    first commit
    first commit

---
+++ src/main/java/com/propelbit/orderservice/OrderDao.java
@@ -0,0 +1,20 @@
+package com.propelbit.orderservice;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Repository
+public class OrderDao {
+
+    public List<Order> getOrders(){
+        return Stream.of(
+                new Order(101,"Mobile",1,30000),
+                new Order(58,"Book",4,30000),
+                new Order(205,"Laptop",1,150000),
+                new Order(301,"Headset",1,1899)
+        ).collect(Collectors.toList());
+    }
+}
